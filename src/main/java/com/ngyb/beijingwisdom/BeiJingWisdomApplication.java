efecad872125068326275b8dcfdd51764ec4cf63
nangongyibin7219 <nangongyibin@gmail.com>
2020-01-06 10:02:48 +0800

    智慧北京后台服务

---
+++ src/main/java/com/ngyb/beijingwisdom/BeiJingWisdomApplication.java
@@ -0,0 +1,13 @@
+package com.ngyb.beijingwisdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeiJingWisdomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeiJingWisdomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ngyb/beijingwisdom/BeiJingWisdomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngyb.beijingwisdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeiJingWisdomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
